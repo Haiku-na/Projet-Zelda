440ba2c20592bc57fef4f62d2e546e521a2d3d2d
Haiku-na <appoline.vautrin@gmail.com>
2025-01-23 09:33:44 +0100

    update image footer

--- src/layout/Footer/Footer.jsx
+++ src/layout/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import { useLocation, useNavigate } from "react-router";
 export default function Footer() {
     return (
       <div className="footer">
-        <img src="Flat_feet_(left).png" alt="Un footer tout ce qu'il y a de plus normal" />
+        <img src="triforce.png" alt="Un footer tout ce qu'il y a de plus normal" />
         <p>&copy; Footer... pied... ok je suis pas drôle. &copy;</p>
       </div>
     );
